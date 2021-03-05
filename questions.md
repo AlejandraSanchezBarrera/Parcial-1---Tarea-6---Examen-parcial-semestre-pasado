bf44a2baa5b239daac8ec03653f765cce5428792
alejandra sanchez barrera <A310401@alumnos.uaslp.mx>
2021-03-04 20:09:27 -0600

    commit

---
+++ questions.md
@@ -0,0 +1,16 @@
+1. Menciona los cuatro pilares de la programación orientada a objetos.
+    > Abstracción, encapsulamiento, herencia, polimorfismo 
+
+1. Se refiere a modelar un sistema tomando en cuenta solo las características de las entidades que afectan al sistema y dejando de lado lo irrelevante
+    > abstracción
+
+1. ¿A que pilar de la POO corresponde el uso de las palabras reservadas public y private ?
+    > encapsulamiento
+
+1. Un objeto tiene estado y comportamiento. Explica a qué se refieren estos dos conceptos (estado y comportamiento).
+    > El estado de un objeto abarca sus propiedades, y los valores actuales de cada propiedad.
+   > El comportamiento es como un objeto actua o reacciona, en términos de sus cambios de estado y de los mensajes que intercambia
+
+1. ¿Con qué palabra reservada de java hacemos que un atributo o un método pernezcan a la clase y no al objeto?
+    > static 
+
---
+++ src/uaslp/enginering/exam/model/Hotel.java
@@ -0,0 +1,50 @@
+package uaslp.enginering.exam.model;
+
+import java.util.ArrayList;
+
+public class Hotel {
+    private ArrayList<Reservation> reservations;
+    private String name;
+    private ArrayList <Room> rooms;
+
+    public Hotel(String name) {
+        this.name=name;
+        reservations = new ArrayList<>();
+        rooms = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /*public void setName(String name) {
+        this.name = name;
+    }*/
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public void addRoom(Room room){
+        rooms.add(room);
+    }
+
+    public void reserveRoom(int roomNumber, Guest guest, String arrivalDate, int nights) {
+        Reservation reservation = new Reservation();
+
+        reservation.setRoomNumber(roomNumber);
+        reservation.setArrivalDate(arrivalDate);
+        reservation.setGuest(guest);
+        reservation.setNights(nights);
+
+        reservations.add(reservation);
+    }
+
+    public ArrayList<Reservation> getReservations() {
+        return reservations;
+    }
+}
